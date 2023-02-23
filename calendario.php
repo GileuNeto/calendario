9a2986bedf6fc06526606664f69217a140a0d8b2
Gileu Neto <gileuneto@gmail.com>
2023-02-23 18:11:56 -0300

    first commit

---
+++ calendario.php
@@ -0,0 +1,67 @@
+<?php
+
+//neste exemplo usei somente um echo
+
+echo '<head>
+<title>Calendario de Dezembro</title>
+<link href="css/styleCalendario.css" rel="stylesheet">
+</head>
+<body>
+<div class="fundo">
+    <div class="calendario">
+        <h1>fevereiro de 2023</h1>
+        <table class="table">
+            <th>D</th> 
+            <th>S</th>
+            <th>T</th>
+            <th>Q</th>
+            <th>Q</th>
+            <th>S</th>
+            <th>S</th>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>  
+                <tr>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td> 
+                    <td>11</td>               
+                </tr>
+                <tr>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                    <td>15</td>
+                    <td>16</td>
+                    <td>17</td>  
+                    <td>18</td>     
+                </tr>
+                <tr>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                    <td>22</td>
+                    <td>23</td>
+                    <td>24</td> 
+                    <td>25</td> 
+                </tr>
+                <tr>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>    
+                </tr>
+                
+        </table>
+        <a href="telaExemplo01.php">Voltar a Pagina Inicial</a>
+    </div>
+</div>
+</body>';
---
+++ telaInicial.php
@@ -0,0 +1,24 @@
+<?php
+
+ //neste exemplo usei somente um echo
+
+   echo('<head>
+   <title>PaginaInicial</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="css/style.css" rel="stylesheet">
+</head>
+   <body>
+<!-- TEXTO DE BOAS VINDAS -->
+
+   <h1>Seja Bem Vindo a pagina inicial</h1>
+
+<!-- LINK PARA O G1(SITE GLOBO) -->
+   <div>
+      <a href="https://g1.globo.com" target="_blank" id="globo">G1 - O portal de notícias da Globo</a>
+   </div>
+<!-- LINK PARA O CALENDARIO -->
+   <div>
+      <a href="tabelaExemplo01.php" id="calendario">Calendario de Dezembro</a>
+   </div>
+</body>');
